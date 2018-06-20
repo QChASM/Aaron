8fd8f7df248216dc88c987faea8aca98d9df1eff
Victoria Ingman <victoria.ingman@uga.edu>
2018-06-20 17:52:55 -0400

    Added relative numbering option for cat_screen

--- bin/_utils.pm
+++ bin/_utils.pm
@@ -1,4 +1,5 @@
 #!/usr/bin/perl -w
+
 =head1 SYNOPSIS
 Provides utilities for common interactions with AaronTools
 =cut
@@ -13,6 +14,7 @@ use Data::Dumper;
 =item get_geom($file)
 Gets geometry object from file
 =cut
+
 sub get_geom {
     use AaronTools::Geometry;
 
@@ -27,40 +29,45 @@ sub get_geom {
     return $geom;
 }
 
-
 =item get_cat($file, \%substituents)
 Gets catalysis object from file. Optional substituent information can be provided.
 
 \%substituents = {'ligand'=>{ atom=>label, ... }, 'substrate'=>{ atom=>label, ... }}
 =cut
+
 sub get_cat {
     use AaronTools::Catalysis;
 
-    my %params;
     my $file = shift;
+
+    my %params;
     $params{substituents} = shift;
     ( $params{name} ) = ( $file =~ /(.*)\..*?$/ );
+
+    my $relnum = shift;
 
     # creat Catalysis object
     my $cat = new AaronTools::Catalysis( 'name' => $params{name} );
 
-    # if substituent info provided, make provided ligand numbering relative
-    if ( $params{substituents} ) {
-		# my $ligstart = $#{ $cat->{coords} } - $#{ $cat->{ligand_atoms} };
-		my $ligstart = get_ligstart($cat);
-		my $substart = get_substart($cat);
+    # if substituent info provided, make atom numbering relative, if necessary
+    if ( $params{substituents} && !($relnum) ) {
+        # this gets the number to subtract for relative numbering
+        my $ligstart = get_ligstart($cat);
+        my $substart = get_substart($cat);
+
+        # update info hash to reflect numbering change
         foreach my $sub ( keys %{ $params{substituents}{ligand} } ) {
             $params{substituents}{ligand}{ $sub - $ligstart } =
-				delete $params{substituents}{ligand}{$sub};
+              delete $params{substituents}{ligand}{$sub};
         }
-		foreach my $sub ( keys %{ $params{substituents}{substrate} } ){
-			$params{substituents}{substrate}{ $sub - $substart } =
-				delete $params{substituents}{substrate}{$sub};
-		};
+        foreach my $sub ( keys %{ $params{substituents}{substrate} } ) {
+            $params{substituents}{substrate}{ $sub - $substart } =
+              delete $params{substituents}{substrate}{$sub};
+        }
 
-        # update catalysis object with substituent info
-        $cat = new AaronTools::Catalysis(%params);
     }
+    # update catalysis object with substituent info, if provided
+    $cat = new AaronTools::Catalysis(%params) if ( $params{substituents} );
 
     # check for success
     unless ( @{ $cat->{elements} } ) {
@@ -73,6 +80,7 @@ sub get_cat {
 =item get_lig($file)
 Reads in ligand object from .xyz file or by built-in name
 =cut
+
 sub get_lig {
     use AaronTools::Catalysis;
 
@@ -95,6 +103,7 @@ Generates an outfile name for printXYZ() methods
 
 outfile = path/filebase_appends.xyz (sep defaults to _)
 =cut
+
 sub get_outfile {
     # prints to STDOUT if $path == ''
     # or saves to infile_append1_append2_etc.xyz
@@ -129,6 +138,7 @@ sub get_outfile {
 =item strip_dir($fname)
 Removes the directory path and returns only the file name
 =cut
+
 sub strip_dir {
     my $fname = shift;
     $fname =~ s/.*\/(.*)/$1/;
@@ -138,17 +148,19 @@ sub strip_dir {
 =item get_ligstart($catalysis)
 Returns a value to be subtracted from an atom index to switch from absolute to relative indexing
 =cut
-sub get_ligstart{
-	my $cat = shift;
-	return (sort {$a <=> $b} @{ $cat->{ligand_atoms} })[0];
+
+sub get_ligstart {
+    my $cat = shift;
+    return ( sort { $a <=> $b } @{ $cat->{ligand_atoms} } )[0];
 }
 
 =item get_substart($catalysis)
 Returns a value to be subtracted from an atom index to switch from absolute to relative indexing
 =cut
-sub get_substart{
-	my $cat = shift;
-	return (sort {$a <=> $b} @{ $cat->{substrate_atoms} })[0];
+
+sub get_substart {
+    my $cat = shift;
+    return ( sort { $a <=> $b } @{ $cat->{substrate_atoms} } )[0];
 }
 
 1;
